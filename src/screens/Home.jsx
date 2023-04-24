a7b39d6a3b6faf926b25123dc0b870744beddcfb
AtikoWeb <126561005+Zero00ne@users.noreply.github.com>
2023-04-25 03:00:06 +0600

    dev

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -95,7 +95,7 @@ const HomeScreen = ({ navigation }) => {
 		} else if (currentHour >= 18 && currentHour < 24) {
 			return 'Добрый вечер';
 		} else {
-			return 'Доброй ночи';
+			return 'Добрая ночь';
 		}
 	};
 
